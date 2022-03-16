608605fcb9ce7c2202a83cc2b00195bd914eb7f6
bxsmash135 <sulaygifrey@gmail.com>
2022-03-16 17:15:47 +0800

    added static simulation env, q learning algorithm, and qlearning interface

--- deep_q_test.py
+++ deep_q_test.py
@@ -1,3 +1,5 @@
+import time
+import threading
 import matplotlib.pyplot as plt
 from pathlib import Path
 import torch
@@ -21,10 +23,10 @@ import traci  # noqa
 
 
 class Net(nn.Module):
-    def __init__(self, total_phases):
+    def __init__(self, states_length, total_phases):
         super().__init__()
         self.total_phases = total_phases
-        self.fc1 = nn.Linear(12,1000)
+        self.fc1 = nn.Linear(states_length,1000)
         self.fc2 = nn.Linear(1000,500)
         self.fc3 = nn.Linear(500,self.total_phases)
 
@@ -38,9 +40,14 @@ class Net(nn.Module):
 
 #Only works for micro simulation (1 traffic light)
 class SumoEnvrionment:
-    def __init__(self, total_phases = 4, gui = True, buffer_size = 12):
-        self.total_phases = total_phases
+    def __init__(self, gui = True, buffer_size = 15, buffer_yellow = 6):
+
+        self.buffer_yellow = buffer_yellow
         self.buffer_size = buffer_size
+        if self.buffer_size < self.buffer_yellow:
+            print("Buffer size must be greater than yellow buffer")
+            raise ValueError
+
         self.gui = gui
 
         self.start_program()
@@ -51,7 +58,8 @@ class SumoEnvrionment:
             self.lanes_dict[lane]=[]
 
         self.traffic_light = traci.trafficlight.getIDList()[0]
-        
+        self.get_phase_data(self.traffic_light)
+
     
     def sumo_initialize(self):
         options = self.get_options()
@@ -68,6 +76,16 @@ class SumoEnvrionment:
             sys.path.append(tools)
         else:
             sys.exit("please declare environment variable 'SUMO_HOME'")
+
+    def get_phase_data(self, traffic_light):
+        """Convert Phase SUMO XML TL Program phase data to native python string phase data
+        """
+        self.tl_program = traci.trafficlight.getProgram(traffic_light)
+        phases_objects=traci.trafficlight.getCompleteRedYellowGreenDefinition(traffic_light)[0].getPhases()
+        self.phases = [phase.state for phase in phases_objects]
+        self.total_phases = len(self.phases)
+        self.previous_tl_state = traci.trafficlight.getRedYellowGreenState(self.traffic_light)
+        
 
     def start_program(self):
         self.sumo_initialize()
@@ -96,7 +114,7 @@ class SumoEnvrionment:
     
     def save_vehicle_ids(self, lanes_dict):
         vehicle_ids=pd.DataFrame(lanes_dict)
-        vehicle_ids['Steps'] = np.arange(0,len(lanes_dict[lane_IDs[0]]),1)
+        vehicle_ids['Steps'] = np.arange(0,len(lanes_dict[self.lane_IDs[0]]),1)
         vehicle_ids.to_csv('test.csv')
 
     def get_reward(self):
@@ -105,9 +123,19 @@ class SumoEnvrionment:
 
         for detector in e2_detectors:
             speed = traci.lanearea.getLastStepMeanSpeed(detector)
+            if speed < 0:
+                speed = 0
             all_speed.append(speed)
-        
-        return np.array(speed).mean()
+
+        population = len(all_speed)
+        all_speed = np.array(all_speed)
+        speed_max = all_speed.max()
+        if speed_max == 0:
+            return 0
+        speed_max_ratio = all_speed/speed_max
+        speed_max_sum = speed_max_ratio.sum()
+
+        return (1/population)*speed_max_sum
 
     def get_state(self, trafficlight):
         e2_detectors = traci.lanearea.getIDList()
@@ -124,72 +152,108 @@ class SumoEnvrionment:
         self.lanes_dict = self.update_lane_data()
         vehicle_ids=pd.DataFrame(self.lanes_dict)
         vehicle_ids['Steps'] = np.arange(0,len(self.lanes_dict[self.lane_IDs[0]]),1)
-        vehicle_ids.to_csv('test.csv')
-        traci.trafficlight.setPhase(self.traffic_light, action)
         
-        for i in range(self.buffer_size):
+        #creates a function that turns green going red to yellow states
+        def change_green_to_yellow(to,prev):
+            green = (prev=='G' or prev=='g')
+            if to=='r' and green:
+                return 'y'
+            else:
+                return prev
+
+        true_state_len = self.buffer_size - self.buffer_yellow
+
+        to_phase = self.phases[action]    # gets current tl state
+        compare = zip(to_phase,self.previous_tl_state)  #zips the current abd previous states for comparison
+        buffer_state = "".join([change_green_to_yellow(t,p) for t,p in compare])
+        
+        traci.trafficlight.setRedYellowGreenState(self.traffic_light, buffer_state)
+        for i in range(self.buffer_yellow):
             traci.simulation.step()
+
+        traci.trafficlight.setRedYellowGreenState(self.traffic_light, self.phases[action])
+        for i in range(true_state_len):
+            traci.trafficlight.setRedYellowGreenState(self.traffic_light, self.phases[action])
+            
+            traci.simulation.step()
+        self.previous_tl_state = to_phase #sets the already finished state as the new prev state
+    
     
     def is_done(self):
         return traci.simulation.getMinExpectedNumber() == 0
 
 
-def train(total_phases = 4,gui=False, train=True, epochs = 80, mem_size = 1000, batch_size = 200, sync_freq = 500, epsilon = 0.2, discount_factor=0.9, learning_rate = 1e-3):
-    gpu = torch.device('cuda:0')
-    cpu = torch.device('cpu:0')
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+def train(gui=False, train=True, debug=False, epochs = 80, mem_size = 1500, batch_size = 800, sync_freq = 500, epsilon = 0.3, discount_factor=0.9, learning_rate = 1e-4):
 
-    net = Net(total_phases)
-    target_net = Net(total_phases)
+    env = SumoEnvrionment(gui=gui)
+    states_length = len(env.get_state(env.traffic_light))
+
+    net = Net(states_length,env.total_phases)
+    target_net = Net(states_length,env.total_phases)
+
     replay = deque(maxlen=mem_size)
+    
+    
+    
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
     loss_fn = nn.MSELoss()
     optimizer = torch.optim.Adam(net.parameters(), lr = learning_rate)
-    losses=[]
-    env = SumoEnvrionment(total_phases = total_phases, gui=gui)
-
+    losses_progression=[]
+    prev_sync = 0
     for i in tqdm(range(epochs), file = sys.stdout):
         state1_ = env.get_state(env.traffic_light)
-        state1 = torch.from_numpy(state1_).float().unsqueeze(dim=0)
+        state1 = torch.from_numpy(state1_).float().unsqueeze(dim=0).to(device)
 
         step = 0
         done = False
-
+        net.to(device)
         while not done:
-            net = net.to(cpu)
-            step += 1
-            qval = net(state1)
-            qval_ = qval.data.numpy()
-            if random.random() < epsilon:
-                action = np.random.randint(0,4)
-            else:
-                action = np.argmax(qval_)
 
+            #For debugging purposes only
+            if debug:
+                if int(traci.simulation.getTime()) > 10000:
+                    break
+
+            qval = net(state1)
+            # qval_ = qval.data.numpy()
+            if random.random() < epsilon:
+                action = np.random.randint(0,env.total_phases)
+            else:
+                action = torch.argmax(qval)
+            
             env.action(action)
             state2_ = env.get_state(env.traffic_light)
-            state2 = torch.from_numpy(state2_).float().unsqueeze(dim=0)
+            state2 = torch.from_numpy(state2_).float().unsqueeze(dim=0).to(device)
             reward = env.get_reward()
             done = env.is_done()
             exp = [state1, action, reward, state2, done]
             replay.append(exp)
+            state1 = state2
 
             if len(replay) > batch_size:
                 minibatch = random.sample(replay, batch_size)
-                state1_batch = torch.cat([s1 for (s1,a,r,s2,d) in minibatch]).to(gpu)
-                action_batch = torch.Tensor([a for (s1,a,r,s2,d) in minibatch]).to(gpu)
-                reward_batch = torch.Tensor([r for (s1,a,r,s2,d) in minibatch]).to(gpu)
-                state2_batch = torch.cat([s2 for (s1,a,r,s2,d) in minibatch]).to(gpu)
-                done_batch = torch.Tensor([d for (s1,a,r,s2,d) in minibatch]).to(gpu)
+                state1_batch = torch.cat([s1 for (s1,a,r,s2,d) in minibatch]).to(device)
+                action_batch = torch.Tensor([a for (s1,a,r,s2,d) in minibatch]).to(device)
+                reward_batch = torch.Tensor([r for (s1,a,r,s2,d) in minibatch]).to(device)
+                state2_batch = torch.cat([s2 for (s1,a,r,s2,d) in minibatch]).to(device)
+                done_batch = torch.Tensor([d for (s1,a,r,s2,d) in minibatch]).to(device)
 
-                net.to(gpu)
+                net.to(device)
                 Q1 = net(state1_batch)
 
-                target_net.to(gpu)
+                target_net.to(device)
                 with torch.no_grad():
                     Q2 = target_net(state2_batch)
                 
                 Y = reward_batch + ((1-done_batch)*discount_factor*torch.max(Q2, dim=1)[0])
                 X = Q1.gather(dim=1,index=action_batch.long().unsqueeze(dim=1)).squeeze()
+
+                # if there are is colliding vehicles, set reward to 0
+                if traci.simulation.getCollidingVehiclesNumber() > 0:
+                    print(f"{traci.simulation.getCollidingVehiclesNumber()} crashed.")
+                    Y = torch.zeros(Y.shape)
 
                 loss = loss_fn(X,Y.detach())
                 
@@ -197,66 +261,180 @@ def train(total_phases = 4,gui=False, train=True, epochs = 80, mem_size = 1000, 
                 loss.backward()
                 optimizer.step()
                 
-                losses.append(loss.item())
-                state1 = state2
+                losses_progression.append([i,loss.item()])
 
-                if step == sync_freq:
+                current_sync = int(traci.simulation.getTime())
+                if (current_sync - prev_sync) > sync_freq:
+                    print(f"Syncs at {int(traci.simulation.getTime())} seconds.")
                     target_net.load_state_dict(net.state_dict())
+                    prev_sync = current_sync
             if done:
                  step = 0
+                 prev_sync = 0
         if gui == False:
             traci.close()
-            traci.start([ "-c", "Simulation_Environment\Main Route Simulation\osm.sumocfg",
-                                "--tripinfo-output", "Data\\tripinfo.xml",  "--start"])
+            traci.start([env.sumoBinary, "-c", "Simulation_Environment\Main Route Simulation\osm.sumocfg",
+                             "--tripinfo-output", "Data\\tripinfo.xml",  "--start"])
         if gui == True:
             traci.close()
             traci.load(["-c", "Simulation_Environment\Main Route Simulation\osm.sumocfg",
                              "--tripinfo-output", "Data\\tripinfo.xml",  "--start"])
 
     target_net.to(torch.device('cpu'))
-    torch.save(target_net, Path('DQN_Model'))
+    local_time="_".join([str(i) for i in list(time.localtime())])
+    torch.save(target_net, Path(f'DQN_Model/Model-{epochs}epochs-_{local_time}.pth'))
     traci.close()
-    return target_net, losses
+    return target_net, losses_progression
 
-def graph_losses(losses, rolling_weight = 50):
+def continue_train(net, last_epoch, gui=False, train=True, debug=False, epochs = 80, mem_size = 1500, batch_size = 800, sync_freq = 500, epsilon = 0.3, discount_factor=0.9, learning_rate = 1e-4):
+    env = SumoEnvrionment(gui = gui)
+    target_net.load_state_dict(net.state_dict())
+
+    replay = deque(maxlen=mem_size)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    loss_fn = nn.MSELoss()
+    optimizer = torch.optim.Adam(net.parameters(), lr = learning_rate)
+    losses_progression=[]
+    prev_sync = 0
+    for i in tqdm(range(epochs), file = sys.stdout):
+        state1_ = env.get_state(env.traffic_light)
+        state1 = torch.from_numpy(state1_).float().unsqueeze(dim=0).to(device)
+
+        step = 0
+        done = False
+        net.to(device)
+        while not done:
+
+            #For debugging purposes only
+            if debug:
+                if int(traci.simulation.getTime()) > 10000:
+                    break
+
+            qval = net(state1)
+            # qval_ = qval.data.numpy()
+            if random.random() < epsilon:
+                action = np.random.randint(0,env.total_phases)
+            else:
+                action = torch.argmax(qval)
+            
+            env.action(action)
+            state2_ = env.get_state(env.traffic_light)
+            state2 = torch.from_numpy(state2_).float().unsqueeze(dim=0).to(device)
+            reward = env.get_reward()
+            done = env.is_done()
+            exp = [state1, action, reward, state2, done]
+            replay.append(exp)
+            state1 = state2
+
+            if len(replay) > batch_size:
+                minibatch = random.sample(replay, batch_size)
+                state1_batch = torch.cat([s1 for (s1,a,r,s2,d) in minibatch]).to(device)
+                action_batch = torch.Tensor([a for (s1,a,r,s2,d) in minibatch]).to(device)
+                reward_batch = torch.Tensor([r for (s1,a,r,s2,d) in minibatch]).to(device)
+                state2_batch = torch.cat([s2 for (s1,a,r,s2,d) in minibatch]).to(device)
+                done_batch = torch.Tensor([d for (s1,a,r,s2,d) in minibatch]).to(device)
+
+                net.to(device)
+                Q1 = net(state1_batch)
+
+                target_net.to(device)
+                with torch.no_grad():
+                    Q2 = target_net(state2_batch)
+                
+                Y = reward_batch + ((1-done_batch)*discount_factor*torch.max(Q2, dim=1)[0])
+                X = Q1.gather(dim=1,index=action_batch.long().unsqueeze(dim=1)).squeeze()
+
+                # if there are is colliding vehicles, set reward to 0
+                if traci.simulation.getCollidingVehiclesNumber() > 0:
+                    print(f"{traci.simulation.getCollidingVehiclesNumber()} crashed.")
+                    Y = torch.zeros(Y.shape)
+
+                loss = loss_fn(X,Y.detach())
+                
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                
+                losses_progression.append([i,loss.item()])
+
+                current_sync = int(traci.simulation.getTime())
+                if (current_sync - prev_sync) > sync_freq:
+                    print(f"Syncs at {int(traci.simulation.getTime())} seconds.")
+                    target_net.load_state_dict(net.state_dict())
+                    prev_sync = current_sync
+            if done:
+                 step = 0
+                 prev_sync = 0
+        if gui == False:
+            traci.close()
+            traci.start([env.sumoBinary, "-c", "Simulation_Environment\Main Route Simulation\osm.sumocfg",
+                             "--tripinfo-output", "Data\\tripinfo.xml",  "--start"])
+        if gui == True:
+            traci.close()
+            traci.load(["-c", "Simulation_Environment\Main Route Simulation\osm.sumocfg",
+                             "--tripinfo-output", "Data\\tripinfo.xml",  "--start"])
+
+    target_net.to(torch.device('cpu'))
+    local_time="_".join([str(i) for i in list(time.localtime())])
+    torch.save(target_net, Path(f'DQN_Model/Model-{last_epoch + epochs}epochs-_{local_time}.pth'))
+    traci.close()
+    return target_net, losses_progression
+
+
+def graph_losses(losses_progress, last_epoch, rolling_weight = 50):
+    losses_progress = np.array(losses_progress)
+    epochs = losses_progress[:,0]
+    losses = losses_progress[:,1]
+
     x = np.arange(0,len(losses),1)
     y = losses
 
-    df = pd.DataFrame({'Epochs':x, 'Losses':y})
-    df['Epochs'] = df['Epochs'].apply(lambda x: f"Epoch {x}")
+    df = pd.DataFrame({'Epochs':epochs, 'Episodes':x, 'Losses':y})
+    df['Epochs'] = df['Epochs'].apply(lambda x: f"Epoch - {x + last_epoch}")
     df['Rolling Losses'] = df['Losses'].rolling(rolling_weight).mean()
+    df['Epoch and Episode'] = df.apply(lambda x: f"{x[0]}: Episode {x[1]}", axis=1)
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(22,10))
 
-    ax.plot(df['Epoch'], df['Losses'], label='Losses')
-    ax.plot(df['Epoch'], df['Rolling Losses'], label='Rolling {rolling_weight} Epoch Average')
+    ax.plot(df['Epoch and Episode'], df['Losses'], label='Losses')
+    ax.plot(df['Epoch and Episode'], df['Rolling Losses'], label=f'Rolling {rolling_weight} Episode Average')
     
-    condition = [True if i % rolling_weight == 0 else False for i in df['Epochs'].to_numpy()]
-    ax.set_xticks(df['Epochs'][condition])
+    ax.set_xticks(df['Epoch and Episode'][np.arange(0,df.shape[0],int(df.shape[0]/5))])
 
     ax.legend()
     plt.show()
+    local_time="_".join([str(i) for i in list(time.localtime())])
+    plt.savefig(Path(f"Figures\Losses\Model-{epochs[-1]}_epochs-_{local_time}"))
 
 def evaluate(target_net):
-    env = SumoEnvrionment(4, gui=True)
+    env = SumoEnvrionment(gui=True)
     is_done = env.is_done()
 
     while not is_done:
-        state_ = env.get_state()
+        state_ = env.get_state(env.traffic_light)
         state = torch.from_numpy(state_).float()
 
         with torch.no_grad():
             qval_ = target_net(state)
         
-        qval = qval_.data.to_numpy()
+        qval = qval_.data.numpy()
         action = np.argmax(qval)
         env.action(action)
         is_done = env.is_done()
-        traci.close()
+    traci.close()
 
 
 
 if __name__ == '__main__':
     target_net, losses = train(epochs=2, gui=False)
-    graph_losses(losses)
-    evaluate(target_net)
+
+    # t1 = threading.Thread(target=graph_losses, args=[losses, 0])
+    t2 = threading.Thread(target=evaluate, args=[target_net])
+
+    # t1.start()
+    t2.start()
+
+    # t1.join()
+    t2.join()
---
+++ main_q_learning.py
@@ -0,0 +1,70 @@
+import os
+from secrets import token_urlsafe
+import deep_q_test as dqn
+from deep_q_test import Net, SumoEnvrionment
+import torch
+
+import os
+
+# os.listdir(os.path.join(os.getcwd(), 'DQN_Model'))
+
+def select_model():
+    models_path = [i for i in os.listdir('DQN_Model') if i.split('.')[-1]=="pth"]
+    text_display = "".join([f"{idx}:    {i} \n" for idx, i in enumerate(models_path)])
+
+    while True:
+        try:
+            print("Select the model:")
+            model_idx = int(input(text_display + "\n"))
+            model = torch.load(os.path.join("DQN_Model",models_path[model_idx]))
+            break
+        except Exception as e:
+            print(e)
+    
+    return model, models_path[model_idx]
+
+if __name__ == '__main__':
+    print("Select the run type:")
+    while True:
+        try:
+            run_type = int(input("0:    New Model\n1:    Continue Train\n2:    Evaluate\n"))
+            if run_type > 2:
+                print("Invalid type. Please Try Again")
+                raise ValueError
+            
+            if run_type == 1:
+                model, path = select_model()
+                epochs = int(input('How Many Epochs?:   '))
+                last_epoch = path.split(r'\\')[-1].split('epochs')[0].split('-')[-1]
+
+                print("Model Successfuly loaded from {}\nLast Epoch: ".format(path, last_epoch))
+                print("Running SUMO...")
+                print('-------------------------------------------------------------------')
+
+                losses, epochs = dqn.continue_train(model, last_epoch=last_epoch, epochs=epochs)
+                
+                print('Running Loss Graph...')
+
+                dqn.graph_losses(losses, last_epoch)
+            elif run_type == 2:
+                model, path = select_model()
+                print("Model Successfuly loaded from {}".format(path))
+                print("Running SUMO...")
+                print('-------------------------------------------------------------------')
+                dqn.evaluate(model)
+            elif run_type == 0:
+                epochs = int(input('How Many Epochs?:   '))
+                target_net, losses = dqn.train(epochs=epochs, gui=False)
+                dqn.evaluate(target_net)
+                dqn.graph_losses(losses)
+                
+            break
+        except Exception as e:
+            print(e)
+            print('Please Try Again....')
+            
+            
+        
+
+
+    
